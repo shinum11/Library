cd43543439d4a53a6773c714702fab354637e5e5
sidnei <sidneigoes23@gmail.com>
2024-05-13 15:55:27 -0300

    Colocando switch case como menu

--- src/main/java/com/mycompany/library/LibraryApp.java
+++ src/main/java/com/mycompany/library/LibraryApp.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.library;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Shinum
@@ -11,7 +13,31 @@ package com.mycompany.library;
 public class LibraryApp {
 
     public static void main(String[] args){
-        System.out.println("Hello World");
-    }   
-    
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Escolha uma das opções:\n1 - Cadrastrar Usuário\n2 - Cadastrar Livro\n3 - Emprestar" +
+                "Livro\n4 - Desvolver Livro\nDigite sua escolha: ");
+
+        int escolha = 0;
+
+        escolha = ler.nextInt();
+
+        switch (escolha){
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+                System.out.println("3");
+                break;
+            case 4:
+                System.out.println("4");
+                break;
+            default:
+                System.out.println("Digite um valor válido!!");
+        }
+
+    }
+
 }
